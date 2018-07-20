030b2a8dfa60b12d0704fd29280fb2891ae41be9
Paul Joo <jianghuiyao@outlook.com>
2018-07-20 17:27:53 +0800

    add index.vue

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,14 @@ export default {
 </script>
 
 <style>
+html {
+    width: 100%;
+    height: 100%;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,19 @@ import Router from 'vue-router'
 
 import login from '@/components/login'
 
+import index from '@/components/index'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
+      name: 'index',
+      component: index
+    },
+    {
+      path: '/login',
       name: 'login',
       component: login
     }
